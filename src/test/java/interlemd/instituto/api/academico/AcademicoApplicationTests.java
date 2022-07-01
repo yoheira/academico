49e9f28721d9b86141b6bd6f634fc7c08f486c7c
yoheira <yoheira@gmail.com>
2022-06-30 20:38:12 -0500

    first commit

---
+++ src/test/java/interlemd/instituto/api/academico/AcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package interlemd.instituto.api.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
